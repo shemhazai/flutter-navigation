01bdc7d397eaa260eb321f0f37392c149026a843
Dominik Toton <dominik.toton@protonmail.com>
2021-05-04 16:44:32 +0200

    Import images

---
+++ lib/common/app/presentation/images.dart
@@ -0,0 +1,19 @@
+class Images {
+  const Images._();
+
+  static const common = _Common();
+  static const home = _HomePage();
+}
+
+class _Common {
+  const _Common();
+
+  String get chevronLeft24 => 'assets/images/ic_chevron_left_24.png';
+  String get chevronRight24 => 'assets/images/ic_chevron_left_24.png';
+}
+
+class _HomePage {
+  const _HomePage();
+
+  String get userAvatar48 => 'assets/images/ic_user_avatar_48.png';
+}
