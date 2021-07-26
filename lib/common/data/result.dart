29f83c1c57817ba12b161334025a5860df94840a
Dominik Toton <dominik.toton@protonmail.com>
2021-07-26 21:17:53 +0200

    Add missing types to the result

--- lib/common/data/result.dart
+++ lib/common/data/result.dart
@@ -3,9 +3,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'result.freezed.dart';
 
 @freezed
-class Result<T, E> with _$Result {
-  const factory Result.success(T data) = SuccessResult;
-  const factory Result.failure(E error) = FailureResult;
+class Result<T, E> with _$Result<T, E> {
+  const factory Result.success(T data) = SuccessResult<T, E>;
+  const factory Result.failure(E error) = FailureResult<T, E>;
 }
 
 extension FutureResult<T, E> on Future<Result<T, E>> {
